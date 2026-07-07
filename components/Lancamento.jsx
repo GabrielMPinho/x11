43d9f3f048f800f1691d752b4d25d59bbe271c2d
Gabriel Pinho <lm.uai@lm2rodas.com.br>
2026-07-07 10:40:35 -0300

    Lançamento

---
+++ components/Lancamento.jsx
@@ -0,0 +1,27 @@
+import imagem from "../src/assets/images/imagem_lancamento.jpg"
+
+export default function Lancamento(){
+    return (
+        <section className="lancamento">
+            <div id="container_escrito">
+                <div id="texto">
+                    <p className="p_laranja">LANÇAMENTO ESPECIAL</p>
+                    <h1 id="titulo_principal">CONCORRA AO COMBRO DE PROTEÇÃO NO BIKE FEST</h1>
+                    <p>Monte o conjunto perfeito para a próxima aventura: escolha uma jaqueta e uma calça e garanta 20% de desconto no look</p>
+                    <button>COMO PARTICIPAR ➜</button>
+                </div>
+            </div>
+            <div id="container_imagem">
+                <img id="imagem_lancamento" src={imagem} alt="imagem lancamento" />
+            </div>
+            
+        </section>
+    )
+}
+
+
+
+
+
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import Hero_Home from "../components/Hero_Home";
 import Favoritos from "../components/Favoritos";
+import Lancamento from "../components/Lancamento";
 
 export default function App(){
     return (
@@ -9,6 +10,7 @@ export default function App(){
             <main>
                 <Hero_Home />
                 <Favoritos />
+                <Lancamento />
             </main>
         </>
     )
